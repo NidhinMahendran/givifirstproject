0651ce8efa76d7cec1900c498e55a005ceb7a27c
Nidhin Mahendran <nidhin@proaibots.com>
2024-06-15 11:54:30 +0530

    final changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine, Column, String, Integer, Text, DateTime, F
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.exc import OperationalError
+from datetime import datetime
 
 # API Key
 api_key = 'AIzaSyDr8ByPJOb0Q5I3ZLB66-PWjW-FSR3o2oU'
@@ -21,6 +22,7 @@ class Channel(Base):
     channel_description = Column(Text)
     channel_status = Column(String(255))
     playlists = relationship("Playlist", back_populates="channel")
+    videos = relationship("Video", back_populates="channel")
 
 class Playlist(Base):
     __tablename__ = 'playlist'
@@ -33,6 +35,7 @@ class Playlist(Base):
 class Video(Base):
     __tablename__ = 'video'
     video_id = Column(String(255), primary_key=True)
+    channel_id = Column(String(255), ForeignKey('channel.channel_id'))
     playlist_id = Column(String(255), ForeignKey('playlist.playlist_id'))
     video_name = Column(String(255))
     video_description = Column(Text)
@@ -45,6 +48,7 @@ class Video(Base):
     duration = Column(Integer)
     thumbnail = Column(String(255))
     caption_status = Column(String(255))
+    channel = relationship("Channel", back_populates="videos")
     playlist = relationship("Playlist", back_populates="videos")
     comments = relationship("Comment", back_populates="video")
 
@@ -58,23 +62,23 @@ class Comment(Base):
     video = relationship("Video", back_populates="comments")
 
 # Database connection
-# DATABASE_URL = "postgresql://user:password@localhost:5432/guvi_project"
+DATABASE_URL = "postgresql://user:password@localhost:5432/guvi_project"
 
-# def check_db_connection():
-#     try:
-#         engine = create_engine(DATABASE_URL)
-#         engine.connect()
-#         Base.metadata.create_all(engine)
-#         Session = sessionmaker(bind=engine)
-#         session = Session()
-#         return session
-#     except OperationalError as e:
-#         st.error(f"Database connection failed: {e}")
-#         return None
+def check_db_connection():
+    try:
+        engine = create_engine(DATABASE_URL)
+        engine.connect()
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        return session
+    except OperationalError as e:
+        st.error(f"Database connection failed: {e}")
+        return None
 
-# session = check_db_connection()
-# if not session:
-#     st.stop()
+session = check_db_connection()
+if not session:
+    st.stop()
 
 # Page title
 st.set_page_config(page_title='YouTube Data Harvesting & Warehousing', page_icon='https://img.icons8.com/ios-filled/50/youtuber.png')
@@ -100,39 +104,94 @@ if 'json_responses' not in st.session_state:
 
 def cache_storage(json):
     try:
-        channel_id = json['items'][0]['snippet']['channelId']
-        channel_name = json['items'][0]['snippet']['channelTitle']
-        channel_description = json['items'][0]['snippet']['description']
+        for channel_id, channel_data in json.items():
+            channel_name = channel_data['items'][0]['snippet']['channelTitle']
+            channel_description = channel_data['items'][0]['snippet']['description']
+            
+            data = {
+                'S.NO': [st.session_state.index],
+                'Channel ID': [channel_id],
+                'Channel Name': [channel_name],
+                'Channel Description': [channel_description]
+            }
+            temp_df = pd.DataFrame(data)
+            
+            st.session_state.channel_df = pd.concat([st.session_state.channel_df, temp_df], ignore_index=True)
+            
+            response_dict = {
+                str(channel_id): channel_data
+            }
+            
+            st.session_state.json_responses.append(response_dict)
+            st.session_state.index += 1
+            
+            # Add to SQL database
+            new_channel = Channel(
+                channel_id=channel_id,
+                channel_name=channel_name,
+                channel_description=channel_description,
+                channel_type='Unknown',  # Assuming this field is not available in the API response
+                channel_views=0,  # Assuming this field is not available in the API response
+                channel_status='Active'  # Assuming a default status
+            )
+            session.add(new_channel)
+            session.commit()
+
+            # Store video details
+            cache_video_storage(channel_data, channel_id)
         
-        data = {
-            'S.NO': [st.session_state.index],
-            'Channel ID': [channel_id],
-            'Channel Name': [channel_name],
-            'Channel Description': [channel_description]
-        }
-        temp_df = pd.DataFrame(data)
-        
-        st.session_state.channel_df = pd.concat([st.session_state.channel_df, temp_df], ignore_index=True)
-        
-        response_dict = {
-            str(channel_id): json
-        }
-        
-        st.session_state.json_responses.append(response_dict)
-        st.session_state.index += 1
-        
-        # Add to SQL database
-        new_channel = Channel(
-            channel_id=channel_id,
-            channel_name=channel_name,
-            channel_description=channel_description,
-            channel_type='Unknown',  # Assuming this field is not available in the API response
-            channel_views=0,  # Assuming this field is not available in the API response
-            channel_status='Active'  # Assuming a default status
-        )
-        # session.add(new_channel)
-        # session.commit()
-        
+    except (IndexError, KeyError) as e:
+        st.error(f"Error processing the response: {e}")
+
+def cache_video_storage(channel_data, channel_id):
+    try:
+        for item in channel_data['items']:
+            video_id = item['id']['videoId']
+            snippet = item['snippet']
+            video_name = snippet['title']
+            video_description = snippet['description']
+            published_date = datetime.strptime(snippet['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
+            thumbnail = snippet['thumbnails']['default']['url']
+
+            # Add to SQL database
+            new_video = Video(
+                video_id=video_id,
+                channel_id=channel_id,
+                video_name=video_name,
+                video_description=video_description,
+                published_date=published_date,
+                view_count=0,  # Assuming initial view count
+                like_count=0,  # Assuming initial like count
+                dislike_count=0,  # Assuming initial dislike count
+                favorite_count=0,  # Assuming initial favorite count
+                comment_count=0,  # Assuming initial comment count
+                duration=0,  # Assuming initial duration
+                thumbnail=thumbnail,
+                caption_status='Unknown'  # Assuming initial caption status
+            )
+            session.add(new_video)
+        session.commit()
+    except (IndexError, KeyError) as e:
+        st.error(f"Error processing the response: {e}")
+
+def cache_comment_storage(video_id, comment_data):
+    try:
+        for item in comment_data:
+            comment_id = item['id']
+            comment_text = item['snippet']['textDisplay']
+            comment_author = item['snippet']['authorDisplayName']
+            comment_published_date = datetime.strptime(item['snippet']['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
+
+            # Add to SQL database
+            new_comment = Comment(
+                comment_id=comment_id,
+                video_id=video_id,
+                comment_text=comment_text,
+                comment_author=comment_author,
+                comment_published_date=comment_published_date
+            )
+            session.add(new_comment)
+        session.commit()
     except (IndexError, KeyError) as e:
         st.error(f"Error processing the response: {e}")
 
@@ -145,20 +204,29 @@ def get_youtube_data(query, max_results=10):
     response = request.execute()
     return response
 
-# def fetch_channels():
-#     channels = session.query(Channel).all()
-#     return pd.DataFrame([(channel.channel_id, channel.channel_name, channel.channel_description) for channel in channels], 
-#                         columns=['Channel ID', 'Channel Name', 'Channel Description'])
+def get_video_comments(video_id, max_results=100):
+    request = youtube.commentThreads().list(
+        videoId=video_id,
+        part='snippet',
+        maxResults=max_results
+    )
+    response = request.execute()
+    return response['items']
 
-# def fetch_videos():
-#     videos = session.query(Video).all()
-#     return pd.DataFrame([(video.video_id, video.video_name, video.video_description, video.published_date, video.view_count, video.like_count, video.dislike_count, video.favorite_count, video.comment_count, video.duration, video.thumbnail, video.caption_status) for video in videos], 
-#                         columns=['Video ID', 'Video Name', 'Video Description', 'Published Date', 'View Count', 'Like Count', 'Dislike Count', 'Favorite Count', 'Comment Count', 'Duration', 'Thumbnail', 'Caption Status'])
+def fetch_channels():
+    channels = session.query(Channel).all()
+    return pd.DataFrame([(channel.channel_id, channel.channel_name, channel.channel_description) for channel in channels], 
+                        columns=['Channel ID', 'Channel Name', 'Channel Description'])
 
-# def fetch_comments():
-#     comments = session.query(Comment).all()
-#     return pd.DataFrame([(comment.comment_id, comment.video_id, comment.comment_text, comment.comment_author, comment.comment_published_date) for comment in comments], 
-#                         columns=['Comment ID', 'Video ID', 'Comment Text', 'Comment Author', 'Comment Published Date'])
+def fetch_videos():
+    videos = session.query(Video).all()
+    return pd.DataFrame([(video.video_id, video.video_name, video.video_description, video.published_date, video.view_count, video.like_count, video.dislike_count, video.favorite_count, video.comment_count, video.duration, video.thumbnail, video.caption_status) for video in videos], 
+                        columns=['Video ID', 'Video Name', 'Video Description', 'Published Date', 'View Count', 'Like Count', 'Dislike Count', 'Favorite Count', 'Comment Count', 'Duration', 'Thumbnail', 'Caption Status'])
+
+def fetch_comments():
+    comments = session.query(Comment).all()
+    return pd.DataFrame([(comment.comment_id, comment.video_id, comment.comment_text, comment.comment_author, comment.comment_published_date) for comment in comments], 
+                        columns=['Comment ID', 'Video ID', 'Comment Text', 'Comment Author', 'Comment Published Date'])
 
 # Tabs for app layout
 tabs = st.tabs(['➕ Add New Channel', '📋 Collected Channels List', '📊 Channel Performance Analytics'])
@@ -172,7 +240,7 @@ with tabs[0]:
             if channel_name:
                 get_channeldetails = get_youtube_data(channel_name)                
                 cache_storage(get_channeldetails)
-                st.success(f'Channel details for "{channel_name}" added.')
+                st.success(f'Channel "{channel_name}" added Successfully.')
             else:
                 st.error("Channel Name: Not provided")
 
@@ -182,23 +250,23 @@ with tabs[1]:
     submit = st.button('Migrate to SQL')
     if submit:
         st.write("Migrating data to SQL...")
-        st.write(st.session_state.json_responses)
-        
-        # for response in st.session_state.json_responses:
-        #     channel_id = list(response.keys())[0]
-        #     json = response[channel_id]
-            # cache_storage(json)
-        # st.success("Data migrated successfully.")
+        for response in st.session_state.json_responses:
+            for channel_id, json in response.items():
+                for video in json['items']:
+                    video_id = video['id']['videoId']
+                    comments = get_video_comments(video_id)
+                    cache_comment_storage(video_id, comments)
+        st.success("Data migrated successfully.")
 
-# with tabs[2]:
-#     st.write('### Channel Details')
-#     channel_df = fetch_channels()
-#     st.dataframe(channel_df)
+with tabs[2]:
+    st.write('### Channel Details')
+    channel_df = fetch_channels()
+    st.dataframe(channel_df)
 
-#     st.write('### Video Details')
-#     video_df = fetch_videos()
-#     st.dataframe(video_df)
+    st.write('### Video Details')
+    video_df = fetch_videos()
+    st.dataframe(video_df)
 
-#     st.write('### Comment Details')
-#     comment_df = fetch_comments()
-#     st.dataframe(comment_df)
+    st.write('### Comment Details')
+    comment_df = fetch_comments()
+    st.dataframe(comment_df)
